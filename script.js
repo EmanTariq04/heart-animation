a2ce30169197e6b4703b4e0f27e365aede5185a4
Eman Tariq <emantariq.1004@gmail.com>
2025-06-17 18:30:26 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const bodyEl = document.querySelector("body");
+
+bodyEl.addEventListener("mousemove", (e) => {
+    const xPos = e.offsetX;
+    const yPos = e.offsetY;
+    const spanEl = document.createElement("span");
+    spanEl.style.left = xPos + "px";
+    spanEl.style.top = yPos + "px";
+    bodyEl.appendChild(spanEl)
+})
